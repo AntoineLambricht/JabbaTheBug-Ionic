a48c0fecfe9519c3448eb896e7ac5e157d288e28
Antoine Lambricht <lambricht@hotmail.com>
2018-02-05 15:28:44 +0100

    Test Qrcode

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,51 +14,12 @@ export class HomePage {
 
   }
 
-  touch(){
-    console.log("Hello");
-    this.color = "brun";
-  
-      this.color = "orange"
-      this.qrScanner.prepare()
-      .then((status: QRScannerStatus) => {
-         if (status.authorized) {
-           // camera permission was granted
-    
-           this.color = "yellow"
-           // start scanning
-           let scanSub = this.qrScanner.scan().subscribe((text: string) => {
-             console.log('Scanned something', text);
-              this.color = "blue"
-             this.qrScanner.hide(); // hide camera preview
-             scanSub.unsubscribe(); // stop scanning
-           });
-    
-           // show camera preview
-           this.qrScanner.show();
-    
-           // wait for user to scan something, then the observable callback will be called
-    
-         } else if (status.denied) {
-            this.color = "red";       
-           // camera permission was permanently denied
-           // you must use QRScanner.openSettings() method to guide the user to the settings page
-           // then they can grant the permission from there
-         } else {
-           this.color = "black";
-           // permission was denied, but not permanently. You can ask for permission again at a later time.
-         }
-      })
-      .catch((e: any) =>this.color = "Error is', "+e); 
-    }
-  
-  }
-
-  /*qrscanner(){
+  qrscanner(){
     this.color = "orange"
     this.qrScanner.prepare()
     .then((status: QRScannerStatus) => {
 
-
+      this.color = "purple"
 
        if (status.authorized) {
          // camera permission was granted
@@ -88,7 +49,7 @@ export class HomePage {
        }
     })
     .catch((e: any) =>this.color = "Error is', "+e); 
-  }*/
+  }
+  
 
-
-
+}
